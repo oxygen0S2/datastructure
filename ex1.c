4c9fdcbfff1baab0702d5ad6c02c2ad37c27261f
jiyoung <0oxgen2@naver.com>
2024-03-14 21:51:17 +0900

    코드 수정

--- ex1.c
+++ ex1.c
@@ -9,7 +9,7 @@ void main()
     float f, f_array[100];
     long l, l_array[100];
 
-    printf("\n char c 크기 = %d\t : char c_array 크기 = %4d", sizeof(c), sizeof(c_array));
+    printf("\n char c 크기 = %d\t: char c_array 크기 = %4d", sizeof(c), sizeof(c_array));
     printf("\n int i 크기 = %d\t: int i_array 크기 = %4d", sizeof(i), sizeof(i_array));
     printf("\n short s 크기 = %d\t: short s_array 크기 = %4d", sizeof(s), sizeof(s_array));
     printf("\n float f 크기 = %d\t: float f_array 크기 = %4d", sizeof(f), sizeof(f_array));
