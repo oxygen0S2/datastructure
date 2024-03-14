af2eb26e65702ae522f13dafb3a8bb8b613c93b4
jiyoung <0oxgen2@naver.com>
2024-03-14 21:53:39 +0900

    오류 수정

--- ex2.c
+++ ex2.c
@@ -11,7 +11,7 @@ void main()
     printf("\n *** 학년별 취득 학점 *** \n\n");
 
     for (i = 0; i < 3; i++) {
-        printf("3d학년 : 총점 = %d, 등급 = %c\n", i + 1, score[i], grade[i]);
+        printf("%3d학년 : 총점 = %d, 등급 = %c\n", i + 1, score[i], grade[i]);
     }
     getchar();
 }
